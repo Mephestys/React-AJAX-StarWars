17bf0698779c74952ede036799ba79c3939644ed
Daniel Abbott <danielabbott@daniel-abbott.com>
2019-06-03 21:23:47 -0600

    Security fix

--- starwars/src/components/InfoSheet/InfoSheet.js
+++ starwars/src/components/InfoSheet/InfoSheet.js
@@ -2,39 +2,56 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "../InfoCard/InfoCard.css";
 
-const InfoSheet = props => {
-  console.log({...props});
-  const character = props;
+class InfoSheet extends React.Component {
+  state = {
+    character: {},
+  };
 
-  return (
-    <div className="InfoCard">
-    <h3><Link to="/">Home</Link></h3>
-      <div className="InfoCard Name">
-        {character.name}
-      </div>
-      <div className="InfoCard Container">
-        <div className="InfoCard Container FieldTitles">
-          GENDER: <br />
-          SKIN COLOR: <br />
-          EYE COLOR: <br />
-          HAIR COLOR: <br />
-          HEIGHT: <br />
-          MASS: <br />
-          BIRTH YEAR: <br />
-        </div>
-        <div className="InfoCard Container Fields">
-          {character.gender} <br />
-          {character.skin_color} <br />
-          {character.eye_color} <br />
-          {character.hair_color} <br />
-          {character.height} <br />
-          {character.mass} <br />
-          {character.birth_year} <br /> 
-        </div>
-      </div>
-    </div>
-  )
+  componentDidMount() {
+    fetch(`https://swapi.co/api/people/${this.props.match.params.id}/`)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log('data results:', data);
+        this.setState({ character: data });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
 
+  render() {
+    const character = this.state.character;
+    return (
+      <div className="InfoCard">
+      <h3><Link to="/">Home</Link></h3>
+        <div className="InfoCard Name">
+          {character.name}
+        </div>
+        <div className="InfoCard Container">
+          <div className="InfoCard Container FieldTitles">
+            GENDER: <br />
+            SKIN COLOR: <br />
+            EYE COLOR: <br />
+            HAIR COLOR: <br />
+            HEIGHT: <br />
+            MASS: <br />
+            BIRTH YEAR: <br />
+          </div>
+          <div className="InfoCard Container Fields">
+            {character.gender} <br />
+            {character.skin_color} <br />
+            {character.eye_color} <br />
+            {character.hair_color} <br />
+            {character.height} <br />
+            {character.mass} <br />
+            {character.birth_year} <br /> 
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default InfoSheet;
